9976ce7c52f2776b522168972659d06832dcfef1
AnthonySzymczak <doyalajs1@live.com>
2021-08-23 12:39:48 -0400

    setup routes.

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,33 @@
+const router = require('express').Router();
+
+//Object's declaration to retrieve, update, delete thoughts, and to add/delete reactions.
+const {
+    getAllThoughts,
+    getThoughtById,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    deleteReaction
+    // requires thought controller
+} = require('../../controllers/thought-controller');
+
+//routes for retrieving all thoughts, and creating thoughts
+router
+    .route('/')
+    .get(getAllThoughts)
+    .post(createThought);
+
+// routes by id, retrieving, updating, and deleting thoughts.
+router
+    .route('/:id')
+    .get(getThoughtById)
+    .put(updateThought)
+    .delete(deleteThought);
+
+//routes to add and delete reactions
+router.route('/:thoughtId/reactions/')
+    .post(addReaction)
+    .delete(deleteReaction)
+
+module.exports = router;
---
+++ routes/api/user-routes.js
@@ -0,0 +1,37 @@
+const router = require('express').Router();
+
+//Object's declaration to retrieve, update, delete users, and to add/delete friends.
+const {
+    getAllUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    deleteFriend
+    //require user controller
+} = require('../../controllers/user-controller');
+
+
+//routes for retrieving all users and creating user
+router
+    .route('/')
+    .get(getAllUsers)
+    .post(createUser);
+
+
+// routes by id, retieve user, update user, delete user
+router.route('/:id')
+    .get(getUserById)
+    .put(updateUser)
+    .delete(deleteUser)
+
+
+//routes to see friends, add friend, delete friend
+router.route('/:userId/friends/:friendId')
+    .post(addFriend)
+    .delete(deleteFriend)
+
+
+
+module.exports = router;
